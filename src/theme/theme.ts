50f5891131d9272385514967b25c301591568c21
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-11-11 15:00:20 -0400

    feat: create theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    primary: '#2A4BA0',
+    white: '#F8F9FB',
+    gray: '#E7ECF0',
+    black: '#1B262E',
+    grayDark: '#616A7D',
+    grayLight: '#E0E2EE',
+  },
+  fonts: {
+    regular: 'Manrope-Regular',
+    semiBold: 'Manrope-SemiBold',
+    bold: 'Manrope-Bold',
+    extraBold: 'Manrope-ExtraBold',
+  },
+};
